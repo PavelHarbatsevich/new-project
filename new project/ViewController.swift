4bc328fd94c96bf0f99ec0a67630cc4174bc6364
Pavel2024GP <harbatsevichpavel36@gmail.com>
2026-09-05 18:21:09 +0300

    Initial Commit

---
+++ new project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  new project
+//
+//  Created by Павел Гарбацевич on 05.09.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
